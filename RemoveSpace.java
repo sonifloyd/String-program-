b65bd940ebe4c169c3c235aa957c92cf4267baef
soni3073@gmail.com <soni3073@gmail.com>
2018-11-02 12:05:58 +0530

    remove space

---
+++ RemoveSpace.java
@@ -0,0 +1,13 @@
+
+/* WAJP TO PRINT A TO Z AND a to z FROM USER*/
+import java.util.Scanner;
+public class RemoveSpace{
+public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    System.out.println("Enter any String");
+    String str=sc.nextLine();
+    char ch[] =str.toCharArray();// convert string int array
+    String nstr="";// create empty string
+
+
+}
